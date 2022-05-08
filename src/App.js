c0a3bf784255c0e86035f172702a1f5163513862
XuanHust <nguyenvietxuan200@gmail.com>
2022-05-08 22:46:51 +0700

    update

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ import { connect } from 'react-redux';
 
 function App(props) {
 
-  const handleLoad = () => {
-    props.callApi()
+  const handleLoad = async () => {
+    const phim = await props.callApi()
   }
 
   useEffect(() => {
